201a3bd1a70a3e29c0ab79bc58ea3f937bfd78b5
Gavriloga <Gavriloga@yandex.ru>
2023-02-10 03:19:05 +0300

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-# display command line options
-
-count=1
-for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
-
-echo "====="
+    echo "Next parameter: $param"
